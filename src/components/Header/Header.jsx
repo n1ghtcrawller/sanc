818d128f6859a55f48b2dc07adc7d87ed94573c1
n1ghtcrawller <borisgr@bk.ru>
2024-07-06 02:02:22 +0300

    [+] 0.0.1 Initilized Web App Uses UI and Calculating price of items Fix the bugs

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Button from "../Button/Button";
 import { useTelegram } from "../../hooks/useTelegram";
 import './Header.css'
 
@@ -7,10 +6,7 @@ const Header = () => {
     const { onClose, tg } = useTelegram();
     return (
         <div className="header">
-            <Button onClick={onClose}>Закрыть</Button>
-            <span className="username">
-                {tg?.initDataUnsafe?.user?.username}
-            </span>
+
         </div>
     );
 };
