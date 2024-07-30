c1b48297a8d35375d8556f77f5e49cffa1a1a333
n1ghtcrawller <borisgr@bk.ru>
2024-07-30 18:14:09 +0300

    [+] 0.0.1.1 Add + and - to product item

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import './ProductItem.css'
-import Button from "../Button/Button";
+import React, { useState } from 'react';
+import './ProductItem.css';
+import Button from "../Button/Button"; // Если он вам нужен, иначе можно удалить
 
+const ProductItem = ({ product, className, onAdd }) => {
+    const [count, setCount] = useState(0); // Состояние для отслеживания количества товаров
 
-const ProductItem = ({product, className, onAdd}) => {
-    const onAddHandler = () =>  {
+    const handleAdd = () => {
+        setCount(count + 1);
         onAdd(product);
-    }
+    };
+
+    const handleRemove = () => {
+        if (count > 0) {
+            setCount(count - 1);
+        }
+    };
+
     return (
         <div className={"product"}>
             <img className={"img"} src={product.img} alt={product.title} />
             <div className={"title"}>{product.title}</div>
-            {/*<div className={"description"}>{product.description}</div>*/}
-            <div className={"price"}></div>
+            <div className={"price"}>
                 <span>₽<b>{product.price}</b></span>
-            <button className={'add-btn'} onClick={onAddHandler}>
-                <h3>В корзину</h3>
-            </button>
+            </div>
+            <div className="counter">
+                <button className={'minus-btn'} onClick={handleRemove}>-</button>
+                <span className="count">{count}</span>
+                <button className={'add-btn'} onClick={handleAdd}>+</button>
+            </div>
         </div>
-
     );
 };
 
