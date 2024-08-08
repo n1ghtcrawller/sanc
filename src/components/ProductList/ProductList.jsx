0ae3bf297d971dd8e6fe2cd59e6fc5d81f35a6a9
n1ghtcrawller <borisgr@bk.ru>
2024-08-08 19:51:48 +0300

    [+] 0.0.3 Added new Design; Refactor Buy button Added Monsterrat serif Bug fixes Added Start Page Added Logo at Start Page and Button "To shop" Added backend Added some styles Bug Fixes for Backend(added query id and user ID)

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -18,7 +18,7 @@ export const getTotalPrice = (items = []) => {
 
 const ProductList = () => {
     const [addedItems, setAddedItems] = React.useState([]);
-    const { tg, queryId, user } = useTelegram();
+    const { tg, queryId, chatId } = useTelegram();
 
     const onSendData = useCallback(() => {
         // Формируем массив с продуктами
@@ -33,7 +33,7 @@ const ProductList = () => {
             products: productsToSend,
             totalPrice: getTotalPrice(addedItems),
             queryId,
-            user, // Добавляем chatId в данные
+            chatId, // Добавляем chatId в данные
         };
 
         fetch('https://keybasicsneutral.ru/web-data', {
