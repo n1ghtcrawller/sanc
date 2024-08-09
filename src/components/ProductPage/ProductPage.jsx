2343a36563e3a9bfad5bd611fcd88b62b32ff195
n1ghtcrawller <borisgr@bk.ru>
2024-08-10 01:18:05 +0300

    [+] 0.0.3 Added new Design; Refactor Buy button Added Monsterrat serif Bug fixes Added Start Page Added Logo at Start Page and Button "To shop" Added backend Added some styles Bug Fixes for Backend(added query id and user ID) Init for pages

--- src/components/ProductPage/ProductPage.jsx
+++ src/components/ProductPage/ProductPage.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { products } from '../ProductList/ProductList';
+import { Link, useParams } from 'react-router-dom';
+import { products, getTotalPrice } from '../ProductList/ProductList';
 import { useTelegram } from '../../hooks/useTelegram';
 import './ProductPage.css';
 
 const ProductPage = () => {
     const { tg } = useTelegram();
+    const { id } = useParams(); // Получаем id из параметров маршрута
+    const product = products.find(p => p.id === parseInt(id)); // Находим продукт по id
     const [count, setCount] = useState(0);
     const [size, setSize] = useState('');
     const [addedItems, setAddedItems] = useState([]);
@@ -17,11 +19,11 @@ const ProductPage = () => {
             tg.MainButton.show();
             tg.MainButton.setParams({
                 text: (`Перейти к оплате ${getTotalPrice(addedItems)} ₽`),
-            });
+        });
         }
     };
 
-    const handleAddItem = (product) => {
+    const handleAddItem = () => {
         if (size) {
             const alreadyAdded = addedItems.find(item => item.product.id === product.id);
             let newItems;
@@ -49,30 +51,30 @@ const ProductPage = () => {
         }
     };
 
+    if (!product) {
+        return <div>Товар не найден</div>; // Обработка случая, если товар не найден
+    }
+
     return (
         <div>
             <Link to="/products" className="back-button">Назад</Link>
-            <div className="product-list">
-                {products.map(product => (
-                    <div key={product.id} className="product-item">
-                        <img src={product.img} alt={product.title} />
-                        <div className="title">{product.title}</div>
-                        <div className="price">₽<b>{product.price}</b></div>
-                        <div className="counter">
-                            <button onClick={() => setCount(count + 1)}>+</button>
-                            <span>{count}</span>
-                            <button onClick={() => setCount(Math.max(count - 1, 0))}>-</button>
-                        </div>
-                        <select onChange={handleSizeChange} value={size}>
-                            <option value="">Выберите размер</option>
-                            <option value="S">S</option>
-                            <option value="M">M</option>
-                            <option value="L">L</option>
-                            <option value="XL">XL</option>
-                        </select>
-                        <button onClick={() => handleAddItem(product)}>Добавить в корзину</button>
-                    </div>
-                ))}
+            <div className="product-item">
+                <img src={product.img} alt={product.title} />
+                <div className="title">{product.title}</div>
+                <div className="price">₽<b>{product.price}</b></div>
+                <div className="counter">
+                    <button onClick={() => setCount(count + 1)}>+</button>
+                    <span>{count}</span>
+                    <button onClick={() => setCount(Math.max(count - 1, 0))}>-</button>
+                </div>
+                <select onChange={handleSizeChange} value={size}>
+                    <option value="">Выберите размер</option>
+                    <option value="S">S</option>
+                    <option value="M">M</option>
+                    <option value="L">L</option>
+                    <option value="XL">XL</option>
+                </select>
+                <button onClick={handleAddItem}>Добавить в корзину</button>
             </div>
         </div>
     );
