d180eb6239e15903045d291eac868e89fdced46a
n1ghtcrawller <borisgr@bk.ru>
2024-08-12 23:05:33 +0300

    [+] 0.0.6 Added some styles and new functons + bug fixed

--- src/components/ProductPage/ProductPage.jsx
+++ src/components/ProductPage/ProductPage.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useParams, useNavigate } from 'react-router-dom';
 import { products } from '../ProductList/ProductList';
 import { useTelegram } from '../../hooks/useTelegram';
@@ -9,14 +9,14 @@ const ProductPage = () => {
     const { tg } = useTelegram();
     const { id } = useParams();
     const navigate = useNavigate();
+    const [inCart, setInCart] = useState(false); // состояние для отслеживания наличия товара в корзине
+
     useEffect(() => {
-        // Показать кнопку сразу при загрузке компонента
         tg.MainButton.show();
         tg.MainButton.setParams({
             text: 'Перейти в корзину'
         });
 
-        // Обработчик для нажатия кнопки
         const onMainButtonClick = () => {
             navigate('/order');
         };
@@ -27,53 +27,87 @@ const ProductPage = () => {
             tg.offEvent('mainButtonClicked', onMainButtonClick);
         };
     }, [tg]);
+
     const product = products.find(p => p.id === parseInt(id));
     const [count, setCount] = useState(0);
     const [size, setSize] = useState('');
-    const { addToCart } = useCart(); // используем хук для доступа к функции добавления
+    const { addToCart } = useCart();
 
     const handleSizeChange = (e) => {
         setSize(e.target.value);
-    }
-
+    };
 
     if (!product) {
         return <div>Товар не найден</div>;
     }
 
-    const isButtonDisabled = count === 0 || !size;
+    const isButtonDisabled = !size;
 
     // Расчет общей стоимости
     const totalPrice = product.price * count;
 
     const handleAddToCart = () => {
-        addToCart(product, count, size, totalPrice); // добавляем товар в корзину
-        navigate('/order');// перенаправляем на страницу заказа
+        setInCart(true); // Устанавливаем состояние, что товар в корзине
+        setCount(count + 1);
+        // navigate('/order');
+    };
+    const handleAddToCartButton = () => {
+        addToCart(product, count, size, totalPrice);
+        navigate('/order');
+    }
+    const handleIncrement = () => {
+        setCount(count + 1);
     };
 
+    const handleDecrement = () => {
+        if (count > 1) {
+            setCount(count - 1);
+        } else {
+            setCount(0);
+            setInCart(false); // Если количество 0, сбрасываем состояние
+        }
+    };
 
     return (
         <div>
-            <Link to="/products" className="back-button">Назад</Link>
+            <button className={'back-button'}>
+            <Link to="/products" className={'text-on-back-button'} >Назад</Link>
+            </button>
             <div className="product-item">
-                <img src={product.img} alt={product.title} />
+                <img src={product.img} alt={product.title}/>
                 <div className="title">{product.title}</div>
                 <div className="price">₽<b>{product.price}</b></div>
-                <div className="counter">
-                    <button onClick={() => setCount(count + 1)}>+</button>
-                    <span>{count}</span>
-                    <button onClick={() => setCount(Math.max(count - 1, 0))}>-</button>
-                </div>
-                <select onChange={handleSizeChange} value={size}>
-                    <option value="">Выберите размер</option>
-                    <option value="S">S</option>
-                    <option value="M">M</option>
-                    <option value="L">L</option>
-                    <option value="XL">XL</option>
-                </select>
-                <div>
-                    <button disabled={isButtonDisabled} onClick={handleAddToCart}>Добавить в корзину</button>
-                </div>
+
+                {/* Логика отображения кнопки и счетчика */}
+                {inCart ? (
+                    <div className={'shopping-info-body'}>
+                    <div className="counter">
+                        <button className={'minus-btn'} onClick={handleDecrement}>-</button>
+                        <span>{count}</span>
+                        <button className={'add-btn'} onClick={handleIncrement}>+</button>
+                    </div>
+                    <div>
+                    <button onClick={handleAddToCartButton} className={'add-to-cart-btn'}>В корзину</button>
+                    </div>
+                    </div>
+
+                ) : (
+                    <>
+                        <select className={'changeSize'} onChange={handleSizeChange} value={size}>
+                            <option value="">Выберите размер</option>
+                            <option value="S">S</option>
+                            <option value="M">M</option>
+                            <option value="L">L</option>
+                            <option value="XL">XL</option>
+                        </select>
+                        <div>
+                            <button disabled={isButtonDisabled} className={'add-to-cart-btn'}
+                                    onClick={handleAddToCart}>Добавить в корзину
+                            </button>
+                        </div>
+                    </>
+                )}
+
                 {/* Отображение общей стоимости */}
                 {count > 0 && (
                     <div className="total-price">
@@ -81,6 +115,7 @@ const ProductPage = () => {
                     </div>
                 )}
             </div>
+            <div className="description">{product.description}</div>
         </div>
     );
 };
