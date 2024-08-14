ca7c3e7ad518a3e6523feb3f97401f0dc81b814c
n1ghtcrawller <borisgr@bk.ru>
2024-08-15 01:48:41 +0300

    [+] 0.0.8 Bug with + and - fix

--- src/components/Order/Order.jsx
+++ src/components/Order/Order.jsx
@@ -96,9 +96,9 @@ const Order = () => {
                         <p>Цена за товар: ₽<b>{item.product.price * item.count}</b></p>
                         <p>Количество: </p>
                         <div className="counter">
-                            <button className="minus-btn" onClick={() => handleDecrement(item.count)}>-</button>
+                            <button onClick={() => handleDecrement(item)}>-</button>
                             <span>{item.count}</span>
-                            <button className="add-btn" onClick={() => handleIncrement(item.count)}>+</button>
+                            <button onClick={() => handleIncrement(item)}>+</button>
                         </div>
                         <button onClick={() => handleRemoveFromCart(item.product.id)} className="remove-button">Удалить</button>
 
